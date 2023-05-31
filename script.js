52d335cdd23e41dcced71c5d099cdf3b0cd2ae33
BradleyThompson10 <brdthompson4@gmail.com>
2023-05-31 18:54:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -363,7 +363,7 @@ if (mark.bmi > john.bmi) {
     console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI ${mark.bmi}`)
 };
 
-*/
+
 
 //example of inefficient code that the loop will fix
 // console.log('lifting weights repetition 1');
@@ -373,5 +373,66 @@ if (mark.bmi > john.bmi) {
 // console.log('lifting weights repetition 5');
 
 for (let rep = 1; rep <= 10; rep++) {
-    console.log('lifting weights repetition 1');
+    console.log(`lifting weights repetition ${rep}`);
 }
+
+
+
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    "teacher",
+    ['Michael', 'Peter', 'Steven'],
+    true
+];
+
+const types = [];
+
+for (let i = 0; i < jonas.length; i++) {
+    // Reading from jonas array 
+    console.log(jonas[i], typeof jonas[i]);
+
+    // Filling types array 
+    // types[i] = typeof jonas[i];
+    types.push(typeof jonas[i]);
+}
+
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+    ages.push(2037 - years[i])
+}
+
+console.log(ages)
+
+//continue and break
+console.log('====ONLY STRINGS====')
+for (let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] !== 'string') continue;
+
+    console.log(jonas[i], typeof jonas[i]);
+}
+
+
+console.log('====BREAK WITH NUMBER====')
+for (let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] === 'number') break;
+
+    console.log(jonas[i], typeof jonas[i]);
+}
+
+*/
+
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    "teacher",
+    ['Michael', 'Peter', 'Steven'],
+];
+
+
